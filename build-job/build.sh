acb95629824da250dcaf13460f9c40def71c7fa2
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-08-28 16:59:31 +0300

    add fix docker

--- build-job/build.sh
+++ build-job/build.sh
@@ -1,13 +1,14 @@
 
+# Fix problem with Docker
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+
 # Build APP and create image
 ./mvnw clean package 
-sudo docker build -t petclinic-app:${BUILD_NUMBER} -f- .<<EOF
+docker build -t petclinic-app:${BUILD_NUMBER} -f- .<<EOF
 FROM openjdk:8-jdk-alpine
 COPY target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar /app.jar
 CMD  [ "java", "-jar", "-Dspring.profiles.active=mysql", "/app.jar"]
 EXPOSE 8080
 EOF
 
-# Fix problem with Docker
-
-sudo chmod 666 /var/run/docker.sock
