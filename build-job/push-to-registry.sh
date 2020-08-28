750a4c7256472f030e6c4096398eaf838a0913b0
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-08-28 16:57:49 +0300

    Update push-to-registry.sh

--- build-job/push-to-registry.sh
+++ build-job/push-to-registry.sh
@@ -8,9 +8,9 @@ check=`git tag --points-at HEAD | wc -l`
 TAG=`git tag --points-at HEAD`
 if [ "$check" -eq 0 ]
 then 
-sudo docker tag petclinic-app:${BUILD_NUMBER} ${REGISTRY}:${BUILD_NUMBER}
-sudo docker push ${REGISTRY}:${BUILD_NUMBER}
+docker tag petclinic-app:${BUILD_NUMBER} ${REGISTRY}:${BUILD_NUMBER}
+docker push ${REGISTRY}:${BUILD_NUMBER}
 else
-sudo docker tag ipetclinic-app:${BUILD_NUMBER} ${REGISTRY}:${TAG}_${BUILD_NUMBER}
-sudo docker push ${REGISTRY}:${TAG}_${BUILD_NUMBER}
+docker tag ipetclinic-app:${BUILD_NUMBER} ${REGISTRY}:${TAG}_${BUILD_NUMBER}
+docker push ${REGISTRY}:${TAG}_${BUILD_NUMBER}
 fi
