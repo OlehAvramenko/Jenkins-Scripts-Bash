9f6e779b40f08a3e11dd1698bb81fe824fa50949
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-08-31 22:37:24 +0300

    add env

--- deploy-job/deploy-to-fargate.reserve.sh
+++ deploy-job/deploy-to-fargate.reserve.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SERVICE=$(aws ecs list-services --cluster Kilo | grep -c Kilo)
+SERVICE=$(aws ecs list-services --cluster ${CLUSTER} | grep -c ${SERVICE_NAME})
 if [[ $SERVICE -gt 0 ]]; then
   echo "Updating service"
-  aws ecs update-service --cluster Kilo --service Kilo-deploy --force-new-deployment
+  aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE_NAME} --force-new-deployment
 else
   echo "Creating task definition"
   tee "fargate-task.json" > "/dev/null" <<EOF
 {
-"family": "kilo-fargate",
-"executionRoleArn": "ecsTaskExecutionRole",
+"family": "${SERVICE_NAME}-fargate",
+"executionRoleArn": "${NAME_ROLE}",
 "networkMode": "awsvpc",
 "containerDefinitions": [
   {
-    "name": "kilo-petclinic",
-    "image": "427050172059.dkr.ecr.us-east-1.amazonaws.com/charlie:latest",
+    "name": "${SERVICE_NAME}-task",
+    "image": "${IMAGE}:latest",
     "portMappings": [
       {
         "containerPort": 8080,
@@ -45,10 +45,10 @@ else
 EOF
 
   echo "Registering task definition"
-  aws ecs register-task-definition --region us-east-1  --cli-input-json file://fargate-task.json
+  aws ecs register-task-definition --region ${REGION}  --cli-input-json file://fargate-task.json
   
   echo "Creating service"
-  REVISION=$(aws ecs describe-task-definition --region us-east-1 --task-definition kilo-fargate --query 'taskDefinition.revision')
-  aws ecs create-service --region us-east-1 --cluster Kilo --service-name Kilo-deploy --task-definition kilo-fargate:"$REVISION" --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-45a4181c],securityGroups=[sg-031cad4dded62d028]}"
+  REVISION=$(aws ecs describe-task-definition --region ${REGION} --task-definition ${SERVICE_NAME}-fargate --query 'taskDefinition.revision')
+  aws ecs create-service --region ${REGION} --cluster ${CLUSTER} --service-name ${SERVICE_NAME} --task-definition ${SERVICE_NAME}-fargate:"$REVISION" --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[${SUBNET_ID}],securityGroups=[${SECURITY_GROUP}]}"
   
 fi
