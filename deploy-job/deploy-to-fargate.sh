c522d758d4f7a227bd74f25779e78729c53ae370
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-08-31 22:42:39 +0300

    change env

--- deploy-job/deploy-to-fargate.sh
+++ deploy-job/deploy-to-fargate.sh
@@ -2,11 +2,11 @@
 
 REVISION=`aws ecs describe-task-definition --region ${REGION} --task-definition ${CLUSTER}-fargate --query 'taskDefinition.revision'`
 
-aws ecs create-service --cluster ${CLUSTER} --region ${REGION} --service-name ${SERVICE_NAME}-${BUILD_NUMBER} --task-definition ${CLUSTER}-fargate:${REVISION} --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-45a4181c],securityGroups=[sg-031cad4dded62d028]}"
+aws ecs create-service --cluster ${CLUSTER} --region ${REGION} --service-name ${SERVICE_NAME}-${BUILD_NUMBER} --task-definition ${CLUSTER}-fargate:${REVISION} --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[${SUBNET_ID}],securityGroups=[${SECURITY_GROUP}]}"
 
 # --------------- CHECK OLD SERVICES AND DELETE IF IT EXISTS ----------------
 
-VERSION=`curl --user aoleh:${TOKEN} ${JENKINS_URL}/job/uniform-deploy/lastSuccessfulBuild/buildNumber`
+VERSION=`curl --user ${JENKINS_USER}:${TOKEN} ${JENKINS_URL}/job/${JOB_NAME}/lastSuccessfulBuild/buildNumber`
 
 aws ecs delete-service --region ${REGION} --cluster ${CLUSTER} --service ${SERVICE_NAME}-${VERSION} --force || \
 echo " ----------- NO ACTIVE SERVICES ---------"
